1d0f94509855f552d56f8ca53a03aca5a7f67622
Drekkou <67373616+Drekkou@users.noreply.github.com>
2025-09-25 11:32:59 +0200

    fin TP1

---
+++ src/personnages/romain.java
@@ -0,0 +1,24 @@
+package personnages;
+
+public class romain {
+	private String nom;
+	private int force;
+
+	public romain(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+
+	private String prendreParole() {
+		return "Le romain" + nom + ":";
+	}
+}
